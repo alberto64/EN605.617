129d239261a6d9453043fca3ddeb8b67420b5041
Your Name <you@example.com>
2021-10-25 20:38:12 -0400

    cleaned up code

--- module8/run.sh
+++ module8/run.sh
@@ -1,10 +1,14 @@
 nvcc matrixOperation.cu -lcublas -o matrix.sh
 nvcc solvingOperation.cu -lcublas -lcusolver -o solver.sh
 
+echo "Matrix Calculations.\n"
+
 ./matrix.sh 4
 ./matrix.sh 64
 ./matrix.sh 264
 ./matrix.sh 
+
+echo "Solver Calculations.\n"
 
 ./solver.sh 4
 ./solver.sh 16
