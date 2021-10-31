852360e3ac51334383ec9d036b047f28013a68ed
Your Name <you@example.com>
2021-10-31 19:15:09 -0400

    Using thrust

---
+++ module9/ThrustAssigment/streamOperations.cu
@@ -0,0 +1,136 @@
+#include <stdio.h>
+#include <time.h>
+#include <thrust/host_vector.h>
+#include <thrust/transform.h>
+#include <thrust/device_vector.h>
+#include <thrust/functional.h>
+#include <thrust/fill.h>
+
+/**
+* add: A method that add two arrays and places the result in a third array using thrust
+*/
+void add(thrust::device_vector<int>& threadCountList, thrust::device_vector<int>& randNumList, thrust::device_vector<int>& resultList) { 
+    thrust::transform(threadCountList.begin(), threadCountList.end(), randNumList.begin(), resultList.begin(), thrust::plus<int>());
+}
+
+/**
+* sub: A method that substract two arrays and places the result in a third array using thrust
+*/
+void sub(thrust::device_vector<int>& threadCountList, thrust::device_vector<int>& randNumList, thrust::device_vector<int>& resultList) { 
+    thrust::transform(threadCountList.begin(), threadCountList.end(), randNumList.begin(), resultList.begin(), thrust::minus<int>());
+}
+
+/**
+* mult: A method that multiplies two arrays and places the result in a third array using thrust
+*/
+void mult(thrust::device_vector<int>& threadCountList, thrust::device_vector<int>& randNumList, thrust::device_vector<int>& resultList) { 
+    thrust::transform(threadCountList.begin(), threadCountList.end(), randNumList.begin(), resultList.begin(), thrust::multiplies<int>());
+}
+
+/**
+* mod: A method that does the modulus between two arrays and places the result in a third thrust
+*/
+void mod(thrust::device_vector<int>& threadCountList, thrust::device_vector<int>& randNumList, thrust::device_vector<int>& resultList) { 
+    thrust::transform(threadCountList.begin(), threadCountList.end(), randNumList.begin(), resultList.begin(), thrust::modulus<int>());
+}
+
+/**
+* printArray: A method that takes in an a label and an array with its size and it feeds it to printf.
+*/
+void printArray(const char* name, thrust::host_vector<int>& array, int size) {
+	printf("\n%s: [ ", name);
+	for (int idx = 0; idx < size; idx++) {
+		printf("%i ", array[idx]);
+	}
+	printf("]\n");
+}
+
+/**
+* runOperations: Taking the number of blocks and threads it does 4 operations on the two 
+* given arrays and prints their results.
+*/
+void runOperations(int numBlocks, int totalThreads) { 
+    
+	// Setup Timing Variables
+	cudaEvent_t start, stop; 
+	float elapsedTimeInMiliseconds; 
+	cudaEventCreate(&start); 
+	cudaEventCreate(&stop); 
+  
+	// Setup host memory variables
+    thrust::host_vector<int> host_threadCountList(totalThreads);
+	thrust::host_vector<int> host_randNumberList(totalThreads);
+
+	// Populate host inputs
+	for(int idx = 0; idx < totalThreads; idx++) { 
+		host_threadCountList[idx] = idx; 
+		host_randNumberList[idx] = rand() % 4; 
+	} 
+
+	// Setup device memory variables
+	thrust::device_vector<int> device_threadCountList = host_threadCountList;
+	thrust::device_vector<int> device_randNumberList = host_randNumberList;
+	thrust::device_vector<int> device_resultList(totalThreads);
+
+	// Turned of to minimize printing
+	printArray("Thread Count", host_threadCountList, totalThreads);
+	printArray("Random Numbers", host_randNumberList, totalThreads);
+
+	// Start event
+	cudaEventRecord(start);
+
+	// Execute each operation and bring result from device to host
+	add(device_threadCountList, device_randNumberList, device_resultList);
+	thrust::host_vector<int> host_addResultList = device_resultList;
+
+	sub(device_threadCountList, device_randNumberList, device_resultList);
+	thrust::host_vector<int> host_subResultList = device_resultList;
+	
+	mult(device_threadCountList, device_randNumberList, device_resultList);
+	thrust::host_vector<int> host_multResultList = device_resultList;
+
+	mod(device_threadCountList, device_randNumberList, device_resultList);
+	thrust::host_vector<int> host_modResultList = device_resultList;
+
+  	cudaEventRecord(stop, 0);
+  	cudaEventSynchronize(stop); 
+  	cudaEventElapsedTime(&elapsedTimeInMiliseconds, start, stop); 
+	printf("Stream and Event Time: %f Miliseconds\n", elapsedTimeInMiliseconds) * 100;
+
+	// Turned of to minimize printing
+	printArray("Add Result", host_addResultList, totalThreads);
+	printArray("Sub Result", host_subResultList, totalThreads);
+	printArray("Mult Result", host_multResultList, totalThreads);
+	printArray("Mod Result", host_modResultList, totalThreads);
+}
+
+int main(int argc, char** argv) {
+	// Based on the work of Andrew Krepps
+	// Set default values in case arguments don't come in command line.
+	int totalThreads = 1024;
+	int blockSize = 256;
+
+	// read command line arguments
+	if (argc >= 2) {
+		totalThreads = atoi(argv[1]);
+	}
+	if (argc >= 3) {
+		blockSize = atoi(argv[2]);
+	}
+
+	int numBlocks = totalThreads/blockSize;
+
+	// validate command line arguments
+	if (totalThreads % blockSize != 0) {
+		++numBlocks;
+		totalThreads = numBlocks*blockSize;
+		
+		printf("Warning: Total thread count is not evenly divisible by the block size\n");
+		printf("The total number of threads will be rounded up to %d\n", totalThreads);
+	}
+	printf("\nTotal Threads: %d\nBlock Size: %d\n", totalThreads, blockSize);
+	
+	runOperations(numBlocks, totalThreads);
+
+	return 0;
+}
